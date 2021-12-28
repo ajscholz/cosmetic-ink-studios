660441370d9f7a973efe398568bd4862ac01704a
ajscholz <yoitsscholzy@gmail.com>
2021-12-28 16:05:36 -0500

    initial build – remove unused pages, build very basic index page

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#a9b7b9`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,22 +6,20 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Hi people!</h1>
+    <p>Welcome to the brand new home of Cosmetic Ink Studios.</p>
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
+      We're in the process of building a new site, so please be patient with us.
+    </p>
+    <p>
+      For now you can see our work or book an consult on{" "}
+      <a
+        href="https://www.instagram.com/cosmeticinkstudios/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Instagram
+      </a>
     </p>
   </Layout>
 )
